447793662e70d139ba3f2ad6169db61c93bba6bb
Marcus Boon <marcus@catalyst-au.net>
2017-10-19 14:24:16 +1000

    Mahara-ise object_status.php

--- object_status.php
+++ object_status.php
@@ -1,55 +1,36 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
  * File status page - stats on where files are b/w local file system and s3
  *
- * @package   module_objectfs
- * @author    Kenneth Hendricks <kennethhendricks@catalyst-au.net>
- * @copyright Catalyst IT
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    mahara
+ * @subpackage module_objectfs
+ * @author     Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once(__DIR__ . '/../../../config.php');
-require_once( __DIR__ . '/lib.php');
-require_once($CFG->libdir.'/adminlib.php');
+define('MENUITEM', 'adminhome/objectfs');
+
+require_once(__DIR__ . '/../../../init.php');
+global $CFG;
+require_once($CFG->docroot . 'module/objectfs/classes/report/objectfs_report.php');
+require_once($CFG->docroot . 'module/objectfs/classes/report/objectfs_report_builder.php');
 
 use module_objectfs\report\objectfs_report;
 use module_objectfs\report\objectfs_report_builder;
 
-
-admin_externalpage_setup('module_objectfs');
-
-$output = $PAGE->get_renderer('module_objectfs');
-
-echo $output->header();
-
-echo $output->heading(get_string('object_status:page', 'module_objectfs'));
-
-echo $output->object_status_page_intro();
+define('TITLE', get_string('object_status:page', 'module.objectfs'));
 
 $reporttypes = objectfs_report::get_report_types();
+$sitedata = array();
 
 foreach ($reporttypes as $reporttype) {
-    $report = objectfs_report_builder::load_report_from_database($reporttype);
-    // We call this render method directly cause 26 cant handle namespace classes.
-    echo $output->render_objectfs_report($report);
+
+    $sitedata[$reporttype] = objectfs_report_builder::load_report_from_database($reporttype);
 }
 
-echo $output->footer();
+$smarty = smarty(array('paginator', 'js/chartjs/Char.min.js'));
+setpageicon($smarty, 'icon-area-chart');
 
+$smarty->assign('sitedata', $sitedata);
 
-
+$smarty->display('module:objectfs:objectfs.tpl');
