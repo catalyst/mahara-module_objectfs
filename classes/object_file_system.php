19c2cb8da9db74835e94d8c5b1111b88cb24e037
Marcus Boon <marcus@catalyst-au.net>
2017-11-02 14:23:29 +1000

    Add aggregate_logger as a require_once

--- classes/object_file_system.php
+++ classes/object_file_system.php
@@ -16,6 +16,7 @@ namespace module_objectfs;
 defined('INTERNAL') || die();
 
 require_once($CFG->docroot . 'module/objectfs/objectfslib.php');
+require_once($CFG->docroot . 'module/objectfs/classes/log/aggregate_logger.php');
 require_once($CFG->docroot . 'module/objectfs/classes/log/null_logger.php');
 require_once($CFG->docroot . 'module/objectfs/classes/log/real_time_logger.php');
 
