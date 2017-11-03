3a544e689aee4f5f932bc4fccb23d3278050739b
Marcus Boon <marcus@catalyst-au.net>
2017-11-03 15:32:34 +1000

    Fix the s3_file_system because it still had Moodle dependencies

--- classes/s3_file_system.php
+++ classes/s3_file_system.php
@@ -1,39 +1,25 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
  * object_file_system abstract class.
  *
  * Remote object storage providers extent this class.
  * At minimum you need to impletment get_remote_client.
  *
- * @package   module_objectfs
- * @author    Kenneth Hendricks <kennethhendricks@catalyst-au.net>
- * @copyright Catalyst IT
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    mahara
+ * @subpackage module_objectfs
+ * @author     Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 namespace module_objectfs;
 
-defined('MOODLE_INTERNAL') || die();
+defined('INTERNAL') || die();
 
 use module_objectfs\object_file_system;
 use module_objectfs\client\s3_client;
 
-require_once($CFG->dirroot . '/admin/tool/objectfs/lib.php');
+require_once($CFG->docroot . 'module/objectfs/objectfslib.php');
+require_once($CFG->docroot . 'module/objectfs/classes/object_file_system.php');
 
 class s3_file_system extends object_file_system {
 
