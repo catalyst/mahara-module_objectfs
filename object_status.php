c0bade333901bcb15c3197f12d716508f6f1ed50
Marcus Boon <marcus@catalyst-au.net>
2017-11-04 20:25:31 +1000

    Format the reports so that we can display them better

--- object_status.php
+++ object_status.php
@@ -26,7 +26,21 @@ $sitedata = array();
 
 foreach ($reporttypes as $reporttype) {
 
-    $sitedata[$reporttype] = objectfs_report_builder::load_report_from_database($reporttype);
+    $report = objectfs_report_builder::load_report_from_database($reporttype);
+
+    switch($reporttype) {
+        case 'location':
+            $sitedata[$reporttype] = format_location_report($report);
+            break;
+        case 'log_size':
+            $sitedata[$reporttype] = format_logsize_report($report);
+            break;
+        case 'mime_type':
+            $sitedata[$reporttype] = format_mimetype_report($report);
+            break;
+    }
+
+    $sitedata[$reporttype] = $report->get_rows();
 }
 
 $smarty = smarty(array('paginator','js/chartjs/Chart.min.js'));
@@ -35,3 +49,81 @@ setpageicon($smarty, 'icon-area-chart');
 $smarty->assign('sitedata', $sitedata);
 
 $smarty->display('module:objectfs:objectfs.tpl');
+
+function format_location_report($report) {
+    $rows = $report->get_rows();
+
+    if (empty($rows)) {
+        return '';
+    }
+
+    foreach ($rows as $row) {
+
+        $row->datakey = get_file_location_string($row->datakey); // Turn int location into string.
+    }
+
+    return $rows;
+}
+
+function format_logsize_report($report) {
+
+	$rows = $report->get_rows();
+
+	if (empty($rows)) {
+		return '';
+	}
+
+	foreach ($rows as $row) {
+
+		$row->datakey = get_size_range_from_logsize($row->datakey); // Turn logsize into a byte range.
+	}
+
+	return $rows;
+}
+
+function format_mimetype_report($report) {
+
+    $rows = $report->get_rows();
+
+	if (empty($rows)) {
+		return '';
+    }
+
+	return $rows;
+}
+
+function get_file_location_string($filelocation) {
+
+    if ($filelocation == 'total') {
+        return get_string('object_status:location:total', 'module.objectfs');
+    }
+
+    switch ($filelocation){
+        case OBJECT_LOCATION_ERROR:
+            return get_string('object_status:location:error', 'module.objectfs');
+        case OBJECT_LOCATION_LOCAL:
+            return get_string('object_status:location:local', 'module.objectfs');
+        case OBJECT_LOCATION_DUPLICATED:
+            return get_string('object_status:location:duplicated', 'module.objectfs');
+        case OBJECT_LOCATION_EXTERNAL:
+            return get_string('object_status:location:external', 'module.objectfs');
+        default:
+            return get_string('object_status:location:unknown', 'module.objectfs');
+    }
+}
+
+function get_size_range_from_logsize($logsize) {
+
+	// Small logsizes have been compressed.
+	if ($logsize == 'small') {
+		return '< 1MB';
+	}
+
+	$floor = pow(2, $logsize);
+	$roof = ($floor * 2);
+	$floor = display_size($floor);
+	$roof = display_size($roof);
+	$sizerange = "$floor - $roof";
+
+	return $sizerange;
+}
