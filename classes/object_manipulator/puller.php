f841f72f025e95c10dc782e3a711cf172c06ac64
Marcus Boon <marcus@catalyst-au.net>
2017-11-05 14:27:04 +1000

    More fixes for SQL and logic for conditionals

--- classes/object_manipulator/puller.php
+++ classes/object_manipulator/puller.php
@@ -50,7 +50,7 @@ class puller extends manipulator {
      */
     public function get_candidate_objects() {
 
-        if (empty($this->sizethreshold)) {
+        if (!empty($this->sizethreshold)) {
 
             $having = 'HAVING MAX(af.size) <= ?
                           AND (o.location = ?)';
@@ -61,7 +61,7 @@ class puller extends manipulator {
             $params = array(OBJECT_LOCATION_EXTERNAL);
         }
 
-        $sql = 'SELECT af.contenhash,
+        $sql = 'SELECT af.contenthash,
                        MAX(af.size) AS filesize
                   FROM artefact_file_files af
              LEFT JOIN artefact a ON af.artefact = a.id
