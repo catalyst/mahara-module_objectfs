08fa43081a321c2ee00de2c2cb80c924915830e1
Marcus Boon <marcus@catalyst-au.net>
2017-11-05 19:51:17 +1000

    Add test CLI file for puller

--- tests/mahara/cli/test_puller.php
+++ tests/mahara/cli/test_puller.php
@@ -17,11 +17,9 @@ require_once(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))) .'/i
 require_once(get_config('libroot') . 'cli.php');
 
 require_once(get_config('docroot') . 'module/objectfs/objectfslib.php');
-require_once(get_config('docroot') . 'module/objectfs/classes/client/s3_client.php');
 require_once(get_config('docroot') . 'module/objectfs/classes/object_manipulator/manipulator.php');
-require_once(get_config('docroot') . 'module/objectfs/classes/object_manipulator/puller.php');
-require_once(get_config('docroot') . 'module/objectfs/classes/log/objectfs_statistic.php');
-require_once(get_config('docroot') . 'artefact/file/lib.php');
+
+use module_objectfs\object_manipulator\manipulator;
 
 $cli = get_cli();
 
@@ -35,42 +33,4 @@ $cli->setup($settings);
 
 $config = get_objectfs_config();
 
-$logger = new \module_objectfs\log\aggregate_logger();
-$filesystem = new \module_objectfs\s3_file_system();
-$manipulator = new puller($filesystem, $config, $logger);
-$candidatehashes = $manipulator->get_candidate_objects();
-
-$i = 0;
-
-foreach ($candidatehashes as $candidatehash) {
-
-    // We only need 10 entries for testing
-    if ($i > 10) {
-        exit(0);
-    }
-
-    // We're only interested in files.
-    if ($candidatehash->artefacttype != 'file') {
-        continue;
-    }
-
-    // Prepare the file artefact.
-    $fileartefact = new \ArtefactTypeFile($candidatehash->artefact);
-
-    if (empty($candidatehash->contenthash)) {
-
-        $candidatehash->contenthash = $fileartefact::generate_content_hash($fileartefact->get_local_path());
-        $fileartefact->save_content_hash();
-    }
-
-    if (!empty($candidatehash->contenthash)) {
-
-        $newlocation = $filesystem->copy_object_from_external_to_local_by_hash($candidatehash->contenthash, $candidatehash->filesize);
-        update_object_record($fileartefact, $newlocation);
-
-        log_info(print_r($newlocation));
-
-        // Increment only if we are pushing.
-        $i++;
-    }
-}
+manipulator::setup_and_run_object_manipulator('puller');
