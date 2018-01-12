b3aa2dd51e8326abd5d18331b2880323e2e00bdb
Bevan Wishart <bevan@catalyst-au.net>
2018-01-12 13:35:17 +1100

    Adapt get_external_client() to get azure client

---
+++ classes/azure_file_system.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * object_file_system abstract class.
+ *
+ * Remote object storage providers extent this class.
+ * At minimum you need to impletment get_remote_client.
+ *
+ * @package    mahara
+ * @subpackage module_objectfs
+ * @author     Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace module_objectfs;
+
+defined('INTERNAL') || die();
+
+use module_objectfs\object_file_system;
+use module_objectfs\client\s3_client;
+
+require_once(get_config('docroot') . 'module/objectfs/objectfslib.php');
+require_once(get_config('docroot') . 'module/objectfs/classes/mahara_external_filesystem.php');
+
+class azure_file_system extends mahara_external_filesystem {
+
+    protected function get_external_client($config) {
+        $azureclient = new azure_client($config);
+        return $azureclient;
+    }
+}
